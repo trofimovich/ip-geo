7178e26fbf6e7fbfd3a1600be76f2ae297643e3a
Eugene Trofimovich <trofimovich.eugene.dev@gmail.com>
2021-03-17 02:01:19 +0100

    Create base components

---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const UserInfo = () => <div>User Info</div>;
+
+export default UserInfo;
---
+++ src/views/Home.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Home = () => <h1>Home View</h1>;
+
+export default Home;
