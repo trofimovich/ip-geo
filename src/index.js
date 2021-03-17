74c9e1bd82d75d2e3af0d78494f574de6c0711d7
Eugene Trofimovich <trofimovich.eugene.dev@gmail.com>
2021-03-17 01:18:54 +0100

    Add eslint

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import "./styles.css";
-
-const App = () => <h1>IP GEO 2 3 4</h1>;
-
-ReactDOM.render(<App />, document.getElementById("root"));
